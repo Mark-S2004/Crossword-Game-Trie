e0fd78cc2293dc3df4a12cce5db47151b3c5e732
Mark Saleh <mark-saleh@outlook.com>
2023-11-24 01:37:10 +0200

    Implement deleteWord Trie member function

--- Trie.cpp
+++ Trie.cpp
@@ -13,3 +13,56 @@ bool Trie::search(const string word)
     }
     return ptr->isEndofWord;
 }
+
+void Trie::deleteAllNodes(nodePointer lastPrefixNode, short lastPrefixCharIndex, nodePointer lastNode, string word)
+{
+    nodePointer prevPtr = lastPrefixNode, ptr = lastPrefixNode->children[word[lastPrefixCharIndex] - 'a'];
+    while (ptr != lastNode)
+    {
+        prevPtr = ptr;
+        ptr = prevPtr->children[word[++lastPrefixCharIndex] - 'a'];
+    }
+    delete ptr;
+    prevPtr->children[word[lastPrefixCharIndex] - 'a'] = NULL;
+    deleteAllNodes(lastPrefixNode, lastPrefixCharIndex, prevPtr, word);
+}
+
+void Trie::deleteWord(const string word)
+{
+    nodePointer ptr = root, lastPrefixNode = NULL;
+    short i, j, count = 0, lastPrefixCharIndex = (word[0] - 'a');
+
+    for (j = 0; j < word.length(); j++)
+    {
+        if (!ptr->children[word[j] - 'a'])
+        {
+            return;
+        }
+        for (i = 0; i < 26; i++)
+            if (ptr->children[i])
+                count++;
+        if (count > 1)
+        {
+            lastPrefixNode = ptr;
+            lastPrefixCharIndex = j;
+        }
+        ptr = ptr->children[word[j] - 'a'];
+    }
+
+    count = 0;
+    for (i = 0; i < 26; i++)
+    {
+        if (ptr->children[i])
+        {
+            count++;
+            break;
+        }
+    }
+
+    if (count)
+        ptr->isEndofWord = false;
+    else if (lastPrefixNode)
+        deleteAllNodes(lastPrefixNode, lastPrefixCharIndex, ptr, word);
+    else
+        deleteAllNodes(root, lastPrefixCharIndex, ptr, word);
+}
--- trie.hpp
+++ trie.hpp
@@ -4,15 +4,6 @@ using namespace std;
 
 class Trie
 {
-public:
-    Trie();
-
-    void insert(const string word);
-
-    bool search(const string word);
-
-    void deleteWord(const string word);
-
 private:
     class Node
     {
@@ -32,4 +23,15 @@ private:
     typedef Node *nodePointer;
 
     nodePointer root;
+
+public:
+    Trie();
+
+    void insert(const string word);
+
+    bool search(const string word);
+
+    void deleteWord(const string word);
+
+    void deleteAllNodes(nodePointer lastPrefixNode, short lastPrefixCharIndex, nodePointer lastNode, string word);
 };
