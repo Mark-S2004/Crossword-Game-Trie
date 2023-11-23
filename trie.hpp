cdc7ea3cda629ebba66ea5732bf8921d885d7400
“Yehia404” <yehiasakr4676@gmail.com>
2023-11-23 23:42:11 +0200

    Develop Trie header file

---
+++ trie.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+class Trie{
+    public:
+
+        Trie();
+
+        void insert(const string word);
+
+        bool search(const string word) const;
+
+        void deleteWord(const string word);
+
+
+    private:
+    
+        class Node{
+            public:
+            bool isEndofWord;
+            Node * children[26];
+
+            Node(): isEndofWord(false){
+                for(int i = 0; i < 26;i++){
+                    children[i] = NULL;
+                }
+            }
+        };
+
+        typedef Node* nodePointer;
+
+        nodePointer myRoot;
+
+};
