eb18012a5bf7b25a19840708b2f730ac5ac491bb
Mark Saleh <mark-saleh@outlook.com>
2023-11-30 12:15:17 +0200

    Fix deleteWord method bugs

--- Trie.cpp
+++ Trie.cpp
@@ -92,22 +92,23 @@ bool Trie::search(const string word)
 void Trie::deleteAllNodes(nodePointer lastPrefixNode, short lastPrefixCharIndex, nodePointer lastNode, string word)
 {
     nodePointer prevPtr = lastPrefixNode, ptr = lastPrefixNode->children[word[lastPrefixCharIndex] - 'a'];
+    short ptrIndex = lastPrefixCharIndex;
     if (lastNode == lastPrefixNode)
         return;
     while (ptr != lastNode)
     {
         prevPtr = ptr;
-        ptr = prevPtr->children[word[++lastPrefixCharIndex] - 'a'];
+        ptr = prevPtr->children[word[++ptrIndex] - 'a'];
     }
     delete ptr;
-    prevPtr->children[word[lastPrefixCharIndex] - 'a'] = NULL;
+    prevPtr->children[word[ptrIndex] - 'a'] = NULL;
     deleteAllNodes(lastPrefixNode, lastPrefixCharIndex, prevPtr, word);
 }
 
 void Trie::deleteWord(const string word)
 {
     nodePointer ptr = root, lastPrefixNode = NULL;
-    short i, j, count = 0, lastPrefixCharIndex = (word[0] - 'a');
+    short i, j, count, lastPrefixCharIndex = (word[0] - 'a');
 
     for (j = 0; j < word.length(); j++)
     {
@@ -115,10 +116,11 @@ void Trie::deleteWord(const string word)
         {
             return;
         }
+        count = 0;
         for (i = 0; i < alphabet_size; i++)
             if (ptr->children[i])
                 count++;
-        if (count > 1)
+        if (count > 1 || ptr->isEndofWord)
         {
             lastPrefixNode = ptr;
             lastPrefixCharIndex = j;
@@ -126,22 +128,10 @@ void Trie::deleteWord(const string word)
         ptr = ptr->children[word[j] - 'a'];
     }
 
-    count = 0;
-    for (i = 0; i < alphabet_size; i++)
-    {
-        if (ptr->children[i])
-        {
-            count++;
-            break;
-        }
-    }
-
-    if (count)
+    if (!ptr->isLeaf())
         ptr->isEndofWord = false;
-    else if (lastPrefixNode)
-        deleteAllNodes(lastPrefixNode, lastPrefixCharIndex, ptr, word);
     else
-        deleteAllNodes(root, lastPrefixCharIndex, ptr, word);
+        deleteAllNodes(lastPrefixNode, lastPrefixCharIndex, ptr, word);
 }
 
 void Trie::displayReq(ostream &out) const
